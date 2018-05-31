c4365d9f438830bfff0997aa0676dd5086639c01
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2018-05-31 10:27:47 -0500

    styled index.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Board from './components/board';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Board />, document.getElementById('root'));
